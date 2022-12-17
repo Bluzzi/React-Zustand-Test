52031011ac36bcd324095b9781f6472ebcf03567
Bluzzi <camilledugas.pro@gmail.com>
2022-12-17 07:01:58 +0100

    _app: create layout

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,8 @@
 import type { AppProps } from "next/app";
-import { ReactElement } from "react";
-import "$libraries/styles/tailwind.css";
 import Head from "next/head";
+import { ReactElement } from "react";
+import { Navbar } from "$libraries/components/navbar";
+import "$libraries/styles/tailwind.css";
 
 export default function App({ Component, pageProps }: AppProps): ReactElement {
   return (
@@ -12,7 +13,15 @@ export default function App({ Component, pageProps }: AppProps): ReactElement {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <Component {...pageProps} />
+      <div className="bg-black h-screen">
+        <div className="fixed w-full my-5 grid place-content-center">
+          <Navbar />
+        </div>
+
+        <div className="h-full grid place-content-center">
+          <Component {...pageProps} />
+        </div>
+      </div>
     </>
   );
 }
