df81abf8a05a81616010ff485d87a6ec2e27b372
Bluzzi <camilledugas.pro@gmail.com>
2022-12-17 06:36:51 +0100

    Setup Next

---
+++ pages/_app.tsx
@@ -0,0 +1,6 @@
+import type { AppProps } from "next/app";
+import { ReactElement } from "react";
+
+export default function App({ Component, pageProps }: AppProps): ReactElement {
+  return <Component {...pageProps} />;
+}
