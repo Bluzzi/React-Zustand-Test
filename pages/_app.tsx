b5ac1e83b3443ec882f60ca50c87f13c20204e2d
Bluzzi <camilledugas.pro@gmail.com>
2022-12-17 06:39:05 +0100

    _app: add favicon and title

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,18 @@
 import type { AppProps } from "next/app";
 import { ReactElement } from "react";
 import "$libraries/styles/tailwind.css";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps): ReactElement {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>React Zustand Test</title>
+
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <Component {...pageProps} />
+    </>
+  );
 }
