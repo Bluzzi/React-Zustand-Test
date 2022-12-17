5236faff1bcee9674d3a18f143b144e4670de235
Bluzzi <camilledugas.pro@gmail.com>
2022-12-17 07:01:33 +0100

    navbar (component): improve page system

--- libraries/components/navbar.tsx
+++ libraries/components/navbar.tsx
@@ -1,13 +1,27 @@
 import Link from "next/link";
+import { clsx } from "clsx";
 import { ReactElement } from "react";
+import { useRouter } from "next/router";
 
 export function Navbar(): ReactElement {
-  const style = "text-white hover:underline";
+  const router = useRouter();
+
+  const pages = [
+    { title: "Home", link: "/" },
+    { title: "Count", link: "/count" }
+  ];
 
   return (
-    <div className="flex gap-3">
-      <Link href="/" className={style}>Home</Link>
-      <Link href="/about" className={style}>About</Link>
+    <div className="flex gap-5">
+      {pages.map(page => (
+        <Link
+          key={page.title}
+          href={page.link}
+          className={clsx("text-white hover:underline", { "underline": router.pathname === page.link })}
+        >
+          {page.title}
+        </Link>
+      ))}
     </div>
   );
 }
